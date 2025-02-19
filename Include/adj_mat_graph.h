f3bae53b99963f3d2848a2214c7d594df280e3b5
Piotr Walczak <106607310+omni-drft@users.noreply.github.com>
2025-02-19 17:07:01 +0100

    Starting to implement graph structure

---
+++ Include/adj_mat_graph.h
@@ -0,0 +1,64 @@
+#ifndef ALGLIB_INCLUDE_ADJMATGRAPH_H_
+#define ALGLIB_INCLUDE_ADJMATGRAPH_H_
+
+#include <array>
+#include <functional>
+#include <vector>
+
+namespace alglib {
+
+template <typename T>
+class AdjMatGraph {
+ public:
+  AdjMatGraph(size_t vertices, bool is_directed);
+  AdjMatGraph(size_t vertices, bool is_directed,
+              const std::vector<std::array<unsigned, 3>>& edges);
+
+  void BFS(size_t start, const std::function<void(int)>& visit_callback);
+  void DFS(size_t start, const std::function<void(int)>& visit_callback);
+
+ private:
+  std::vector<std::vector<int>> adjacency_matrix;
+  bool directed;
+};
+
+template <typename T>
+AdjMatGraph<T>::AdjMatGraph(size_t vertices, bool is_directed)
+    : directed(is_directed) {
+  adjacency_matrix.resize(vertices);
+  for (auto in_vec : adjacency_matrix) {
+    in_vec.resize(vertices);
+  }
+}
+
+template <typename T>
+AdjMatGraph<T>::AdjMatGraph(size_t vertices, bool is_directed,
+                            const std::vector<std::array<unsigned, 3>>& edges)
+    : directed(is_directed) {
+  adjacency_matrix.resize(vertices);
+  for (std::vector<int> in_vec : adjacency_matrix) {
+    in_vec.resize(vertices);
+  }
+  for (auto pair : edges) {
+    if (directed) {
+      adjacency_matrix.at(pair.at(0)).at(pair.at(1)) = pair.at(2);
+    } else {
+      adjacency_matrix.at(pair.at(0)).at(pair.at(1)) = pair.at(2);
+      adjacency_matrix.at(pair.at(1)).at(pair.at(0)) = pair.at(2);
+    }
+  }
+}
+
+template <typename T>
+void AdjMatGraph<T>::BFS(size_t start,
+                         const std::function<void(int)>& visit_callback) {
+}
+
+template <typename T>
+void AdjMatGraph<T>::DFS(size_t start,
+                         const std::function<void(int)>& visit_callback) {
+}
+
+}  // namespace alglib
+
+#endif  // ALGLIB_INCLUDE_ADJMATGRAPH_H_
